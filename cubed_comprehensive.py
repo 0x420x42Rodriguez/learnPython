d1903bbfad3426af639e5eded46a3b5038ced2c5
ChrisM <medic9r1@gmail.com>
2019-10-04 14:35:36 -0500

    Python Course Exercises

---
+++ cubed_comprehensive.py
@@ -0,0 +1,6 @@
+#Print the first 10 cubes with a comprehensive statement
+#04 Oct 2019
+#Chris Menne
+
+cubes = [value**3 for value in range(1, 11)]
+print(cubes)
---
+++ pizza.py
@@ -0,0 +1,7 @@
+#Chris Menne
+#04 October 2019
+#practicing FOR loops and discussing pizza
+pizzas = ["pepperoni", "meatlovers", "garlic chicken"]
+for pizza in pizzas:
+	print(f"I love {pizza.title()}.\n")
+print("I really love pizza!")
